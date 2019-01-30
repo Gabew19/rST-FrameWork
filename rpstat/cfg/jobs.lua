5ee6b8f4a3e0127c425b90f684b5686900f37cb2
Gabew19 <47000024+Gabew19@users.noreply.github.com>
2019-01-30 16:01:33 -0500

    Added Jobs, More to come!

--- rpstat/cfg/jobs.lua
+++ rpstat/cfg/jobs.lua
@@ -1,15 +1,46 @@
 
+local.cfg = ()
 
-
-
-
-
-
-createJob = "State Trooper"
-pay = $1000, 1000ms -- How often pay happens.
-
-RegisterEvents = 
-"cuff"
-"drag"
-"text"
-"jail"
+cfg.jobs = {
+  ["State Trooper"] = {
+  config = {onSpawn = function(player) rSTclient.notify(player,{"You are a trooper"}) end},
+  "cuff"
+  "drag"
+  "text"
+  "jail"
+},
+ ["LSPD Officer"] = {
+  config = {onSpawn = function(player) rSTclient.notify(player,{"You are a officer"}) end},
+  "cuff"
+  "drag"
+  "text"
+  "jail"
+},
+ ["Blaine County Sheriff"] = {
+  config = {onSpawn = function(player) rSTclient.notify(player,{"You are a sheriff"}) end},
+  "cuff"
+  "drag"
+  "text"
+  "jail"
+},
+ ["LSPD Chief"] = {
+  config = {onSpawn = function(player) rSTclient.notify(player,{"You are a chief"}) end},
+  "cuff"
+  "drag"
+  "text"
+  "jail"
+},
+ ["State Trooper Commisoner"] = {
+  config = {onSpawn = function(player) rSTclient.notify(player,{"You are a cop"}) end},
+  "cuff"
+  "drag"
+  "text"
+  "jail"
+},
+ ["State Trooper"] = {
+  config = {onSpawn = function(player) rSTclient.notify(player,{"You are a cop"}) end},
+  "cuff"
+  "drag"
+  "text"
+  "jail"
+},
